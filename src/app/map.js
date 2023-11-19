49b9c996bfc8a314696ef5a657af45ba985ee2d4
bryan <bryanhuang66@gmail.com>
2023-11-19 09:18:11 -0800

    use client fix

--- src/app/map.js
+++ src/app/map.js
@@ -1,12 +1,27 @@
-import MapLogic from './maplogic';
+"use client";
+import { useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
 
-const YourComponent = () => {
-  return (
-    <div>
-      <h1>Your Next.js Mapbox App</h1>
-      <MapLogic />
-    </div>
-  );
+const initializeMap = () => {
+  mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
+
+  const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9, // starting zoom
+  });
+
+  // Clean up on unmount
+  return () => map.remove();
 };
 
-export default YourComponent;
+const Map = () => {
+  useEffect(() => {
+    initializeMap();
+  }, []); // Run only once on mount
+
+  return <div id="map" style={{ width: '100%', height: '400px' }} />;
+};
+
+export default Map;
