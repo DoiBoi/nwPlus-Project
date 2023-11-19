7006b548b257fb609118dd5c99bd91cfaa80e17f
bryan <bryanhuang66@gmail.com>
2023-11-19 15:29:42 -0800

    page update

--- src/app/page.js
+++ src/app/page.js
@@ -20,19 +20,29 @@ export default function Home() {
         <Navbar />
 
         {!showMap && (
-          <form className="flex flex-col items-center my-[30px]" onSubmit={handleButtonClick}>
+          <form className="flex flex-col items-center justify-center h-full" onSubmit={handleButtonClick}>
             <div className='flex justify-center'>
               <input
                 id="big-button"
                 type="submit"
                 value="Find washrooms near me!"
-                style={{ backgroundColor: 'white', borderRadius: '8px', padding: '10px 20px', cursor: 'pointer', marginBottom: '20px' }}
+                className="font-bold"
+                style={{
+                  backgroundColor: 'white',
+                  borderRadius: '8px',
+                  padding: '10px 20px',
+                  cursor: 'pointer',
+                  marginBottom: '50px',
+                  marginTop: '50px',
+                  boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)', // Add this line for the shadow
+                }}
               />
+
             </div>
 
             <div className='flex flex-col justify-center px-[10%] py-[10%]' style={{ backgroundColor: 'rgba(255, 255, 255, 0.9)', borderRadius: '10px', padding: '20px', marginBottom: '20px' }}>
               <div id="filters">
-                <h2 className='mb-4 font-semibold'>Filters</h2>
+                <h2 className='mb-4 font-semibold'>Accessibility Filters</h2>
                 {/* This is where the other options are */}
                 <input
                   type="checkbox"
@@ -42,7 +52,7 @@ export default function Home() {
                   checked={wheelAccess}
                   onChange={() => setWheelAccess(!wheelAccess)}
                 />
-                <label htmlFor="wheel_access" style={{ fontSize: '14px' }}> Wheel accessible?</label>
+                <label htmlFor="wheel_access" style={{ fontSize: '14px' }}> Require wheelchair accessiblity?</label>
 
                 <br />
 
@@ -54,7 +64,7 @@ export default function Home() {
                   checked={twentyFourHour}
                   onChange={() => setTwentyFourHour(!twentyFourHour)}
                 />
-                <label htmlFor="24_hour" style={{ fontSize: '14px' }}> All day availability?</label>
+                <label htmlFor="24_hour" style={{ fontSize: '14px' }}> Requir 24 hour availability?</label>
 
               </div>
             </div>
