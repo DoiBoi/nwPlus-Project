61151376202941a7d1ab010511a61382b75f3bef
bryan <bryanhuang66@gmail.com>
2023-11-19 11:45:47 -0800

    map single instance

--- src/app/map.js
+++ src/app/map.js
@@ -2,53 +2,29 @@ import { useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 
 const Map = () => {
+    const [map, setMap] = useState(null);
     const [data, setData] = useState(null);
 
     const initializeMap = (data) => {
         mapboxgl.accessToken = 'pk.eyJ1IjoiY2FsY2l1bS1kb2kiLCJhIjoiY2xwNXBxZnI5MWh1bTJqbzh2bW81bW4xNyJ9.lmPvaF2IOnm9glibmNPrFw';
 
-        const map = new mapboxgl.Map({
-            container: 'map', // container ID
-            style: 'mapbox://styles/mapbox/streets-v11', // style URL
-            center: [-123.1216, 49.2827], // Vancouver, BC coordinates [lng, lat]
-            zoom: 9, // starting zoom
-        });
-
-        addPointsToMap(map, data);
-        return () => map.remove();
-    };
-
-    const addPoint = (map, coord, id, washroomProperties) => {
-        map.on('load', () => {
-            map.addSource(`point-${id}`, {
-                type: 'geojson',
-                data: {
-                    type: 'FeatureCollection',
-                    features: [
-                        {
-                            type: 'Feature',
-                            geometry: {
-                                type: 'Point',
-                                coordinates: coord,
-                            },
-                            properties: washroomProperties,
-                        },
-                    ],
-                },
+        // Check if map already exists
+        if (!map) {
+            const newMap = new mapboxgl.Map({
+                container: 'map', // container ID
+                style: 'mapbox://styles/mapbox/streets-v11', // style URL
+                center: [-123.1216, 49.2827], // Vancouver, BC coordinates [lng, lat]
+                zoom: 9, // starting zoom
             });
 
-            map.addLayer({
-                id: `point-${id}`,
-                type: 'circle',
-                source: `point-${id}`,
-                paint: {
-                    'circle-radius': 4,
-                    'circle-color': '#FF0000',
-                },
-            });
-        });
-    };
+            setMap(newMap);
+        }
 
+        // Update map data
+        if (map && data) {
+            addPointsToMap(map, data);
+        }
+    };
 
     const addPointsToMap = (map, washrooms) => {
         let id = 0;
@@ -56,6 +32,35 @@ const Map = () => {
             let coord = [washroom.geo_point_2d.lon, washroom.geo_point_2d.lat];
             addPoint(map, coord, id.toString(), washroom);
             id = id + 1;
+        });
+    };
+
+    const addPoint = (map, coord, id, washroomProperties) => {
+        map.addSource(`point-${id}`, {
+            type: 'geojson',
+            data: {
+                type: 'FeatureCollection',
+                features: [
+                    {
+                        type: 'Feature',
+                        geometry: {
+                            type: 'Point',
+                            coordinates: coord,
+                        },
+                        properties: washroomProperties,
+                    },
+                ],
+            },
+        });
+
+        map.addLayer({
+            id: `point-${id}`,
+            type: 'circle',
+            source: `point-${id}`,
+            paint: {
+                'circle-radius': 4,
+                'circle-color': '#FF0000',
+            },
         });
     };
 
@@ -79,11 +84,7 @@ const Map = () => {
         }
     }, [data]);
 
-
-
     return <div id="map" style={{ width: '100%', height: '400px' }} />;
 };
 
 export default Map;
-
-
