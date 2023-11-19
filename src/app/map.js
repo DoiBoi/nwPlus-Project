1424d26944c8b12e4adb28ce6d5d60a55f6fbb48
bryan <bryanhuang66@gmail.com>
2023-11-19 09:07:19 -0800

    general structure

---
+++ src/app/map.js
@@ -0,0 +1,12 @@
+import MapLogic from './maplogic';
+
+const YourComponent = () => {
+  return (
+    <div>
+      <h1>Your Next.js Mapbox App</h1>
+      <MapLogic />
+    </div>
+  );
+};
+
+export default YourComponent;
