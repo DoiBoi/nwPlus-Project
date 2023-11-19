95c41bfb538ce11c9688bc7de34b6e5eee00e358
bryan <bryanhuang66@gmail.com>
2023-11-19 10:00:18 -0800

    read json

--- src/app/map.js
+++ src/app/map.js
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
 import mapboxgl from 'mapbox-gl';
-
 const initializeMap = () => {
     mapboxgl.accessToken = 'pk.eyJ1IjoiY2FsY2l1bS1kb2kiLCJhIjoiY2xwNXBxZnI5MWh1bTJqbzh2bW81bW4xNyJ9.lmPvaF2IOnm9glibmNPrFw';
 
@@ -10,26 +9,26 @@ const initializeMap = () => {
         center: [-123.1216, 49.2827], // Vancouver, BC coordinates [lng, lat]
         zoom: 9, // starting zoom
     });
-    // Clean up on unmount
+
+
     return () => map.remove();
 };
 
-const getWashrooms = () => {
-    
-}
-
-
-
 const Map = () => {
-    const [washrooms, setWashrooms] = useState(null);
-
-
     useEffect(() => {
-        const cleanupMap = initializeMap();
-        setWasherooms(getWashrooms);
-        return cleanupMap;
-    }, []); // Run only once on mount
+        const fetchData = async () => {
+            try {
+                const response = await fetch('/public-washrooms.json');
+                const data = await response.json();
 
+                const cleanupMap = initializeMap();
+                return cleanupMap;
+            } catch (error) {
+                console.error('Error fetching JSON data:', error);
+            }
+        };
+        fetchData();
+    }, []);
     return <div id="map" style={{ width: '100%', height: '400px' }} />;
 };
 
