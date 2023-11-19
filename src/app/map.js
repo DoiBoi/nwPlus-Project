c29c262a7e1eb3bcbd89a0544c6a8270b717b5e4
bryan <bryanhuang66@gmail.com>
2023-11-19 10:59:16 -0800

    add points of washrooms

--- src/app/map.js
+++ src/app/map.js
@@ -1,96 +1,89 @@
 import { useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 
-
-const initializeMap = (data) => {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiY2FsY2l1bS1kb2kiLCJhIjoiY2xwNXBxZnI5MWh1bTJqbzh2bW81bW4xNyJ9.lmPvaF2IOnm9glibmNPrFw';
-
-    const map = new mapboxgl.Map({
-        container: 'map', // container ID
-        style: 'mapbox://styles/mapbox/streets-v11', // style URL
-        center: [-123.1216, 49.2827], // Vancouver, BC coordinates [lng, lat]
-        zoom: 9, // starting zoom
-    });
-
-    addPointsToMap(map, data);
-
-
-
-
-    return () => map.remove();
-};
-
-const addPoint = (map, coord) => {
-    map.on('load', () => {
-        map.addSource('point', {
-            type: 'geojson',
-            data: {
-                type: 'FeatureCollection',
-                features: [
-                    {
-                        type: 'Feature',
-                        geometry: {
-                            type: 'Point',
-                            coordinates: coord // Longitude and latitude of the point
-                        },
-                        properties: {
-                            title: 'My Point',
-                            description: 'This is a sample point.'
-                        }
-                    }
-                ]
-            }
-        });
-
-        // Add a layer to render the point on the map
-        map.addLayer({
-            id: 'point',
-            type: 'circle',
-            source: 'point',
-            paint: {
-                'circle-radius': 8,
-                'circle-color': '#FF0000' // Red color for the point
-            }
-        });
-
-        // Create a popup for the point
-        const popup = new mapboxgl.Popup({
-            closeButton: false,
-            closeOnClick: false
-        });
-
-        // Display a popup when the point is clicked
-        map.on('mouseenter', 'point', (e) => {
-            const coordinates = e.features[0].geometry.coordinates.slice();
-            const { title, description } = e.features[0].properties;
-
-            popup.setLngLat(coordinates)
-                .setHTML(`<h3>${title}</h3><p>${description}</p>`)
-                .addTo(map);
-        });
-
-        // Hide the popup when the mouse leaves the point
-        map.on('mouseleave', 'point', () => {
-            popup.remove();
-        });
-    });
-}
-
-const addPointsToMap = (map, washrooms) => {
-    // addPoint(map, [-123.103599022256, 49.2778209665246])
-    console.log(washrooms);
-    for (const washroom in washrooms) {
-        // console.log(washroom[0].name);
-        let coord = [washroom.geo_point_2d.lon, washroom.geo_point_2d.lat]
-        alert(coord);
-        addPoint(map, coord);
-
-    }
-}
-
-
 const Map = () => {
     // const [washrooms, setWashrooms] = useState([])
+
+    const initializeMap = (data) => {
+        mapboxgl.accessToken = 'pk.eyJ1IjoiY2FsY2l1bS1kb2kiLCJhIjoiY2xwNXBxZnI5MWh1bTJqbzh2bW81bW4xNyJ9.lmPvaF2IOnm9glibmNPrFw';
+
+        const map = new mapboxgl.Map({
+            container: 'map', // container ID
+            style: 'mapbox://styles/mapbox/streets-v11', // style URL
+            center: [-123.1216, 49.2827], // Vancouver, BC coordinates [lng, lat]
+            zoom: 9, // starting zoom
+        });
+
+        addPointsToMap(map, data);
+        return () => map.remove();
+    };
+
+    const addPoint = (map, coord, id) => {
+        map.on('load', () => {
+          map.addSource(`point-${id}`, {
+            type: 'geojson',
+            data: {
+              type: 'FeatureCollection',
+              features: [
+                {
+                  type: 'Feature',
+                  geometry: {
+                    type: 'Point',
+                    coordinates: coord,
+                  },
+                  properties: {
+                    title: 'My Point',
+                    description: 'This is a sample point.',
+                  },
+                },
+              ],
+            },
+          });
+      
+          // Add a layer to render the point on the map
+          map.addLayer({
+            id: `point-${id}`,
+            type: 'circle',
+            source: `point-${id}`,
+            paint: {
+              'circle-radius': 8,
+              'circle-color': '#FF0000', // Red color for the point
+            },
+          });
+      
+          // Create a popup for the point
+          const popup = new mapboxgl.Popup({
+            closeButton: false,
+            closeOnClick: false,
+          });
+      
+          // Display a popup when the point is clicked
+          map.on('mouseenter', `point-${id}`, (e) => {
+            const coordinates = e.features[0].geometry.coordinates.slice();
+            const { title, description } = e.features[0].properties;
+      
+            popup
+              .setLngLat(coordinates)
+              .setHTML(`<h3>${title}</h3><p>${description}</p>`)
+              .addTo(map);
+          });
+      
+          // Hide the popup when the mouse leaves the point
+          map.on('mouseleave', `point-${id}`, () => {
+            popup.remove();
+          });
+        });
+      };
+      
+      const addPointsToMap = (map, washrooms) => {
+        let id = 0;
+        washrooms.forEach((washroom) => {
+          let coord = [washroom.geo_point_2d.lon, washroom.geo_point_2d.lat];
+          addPoint(map, coord, id.toString());
+          id = id + 1;
+        });
+      };
+      
 
     useEffect(() => {
         const fetchData = async () => {
@@ -102,11 +95,16 @@ const Map = () => {
             }
         };
 
-        const data = fetchData();
-        const cleanupMap = initializeMap(data.json());
+        const fetchDataAndInitializeMap = async () => {
+            const data = await fetchData();
+            initializeMap(data);
+        };
 
-        return cleanupMap;
+        fetchDataAndInitializeMap();
     }, []);
+
+
+
     return <div id="map" style={{ width: '100%', height: '400px' }} />;
 };
 
