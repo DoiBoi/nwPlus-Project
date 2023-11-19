8da7e5bacbb3e433768ac72e380b87216bd8deb8
bryan <bryanhuang66@gmail.com>
2023-11-19 09:21:10 -0800

    map logic and map merge

--- src/app/map.js
+++ src/app/map.js
@@ -1,27 +1,26 @@
-"use client";
 import { useEffect } from 'react';
 import mapboxgl from 'mapbox-gl';
 
 const initializeMap = () => {
-  mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
+    mapboxgl.accessToken = 'pk.eyJ1IjoiY2FsY2l1bS1kb2kiLCJhIjoiY2xwNXBxZnI5MWh1bTJqbzh2bW81bW4xNyJ9.lmPvaF2IOnm9glibmNPrFw';
 
-  const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9, // starting zoom
-  });
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: [-74.5, 40], // starting position [lng, lat]
+        zoom: 9, // starting zoom
+    });
 
-  // Clean up on unmount
-  return () => map.remove();
+    // Clean up on unmount
+    return () => map.remove();
 };
 
 const Map = () => {
-  useEffect(() => {
-    initializeMap();
-  }, []); // Run only once on mount
+    useEffect(() => {
+        initializeMap();
+    }, []); // Run only once on mount
 
-  return <div id="map" style={{ width: '100%', height: '400px' }} />;
+    return <div id="map" style={{ width: '100%', height: '400px' }} />;
 };
 
 export default Map;
