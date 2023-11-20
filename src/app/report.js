4a5f612dd246008d239ae58a003aa9a34541f5cb
Vassily-Petrousevitch <vasspetrousevitch@gmail.com>
2023-11-19 17:39:55 -0800

    minor fix for error message

--- src/app/report.js
+++ src/app/report.js
@@ -5,11 +5,11 @@ const Report = (props) => {
 
   return (
     <div id="report">
-      <div className="flex flex-col justify-center align-center">
+      <div className="fixed flex-col justify-center align-center">
           <h1 className="flex text-5xl mb-[10px]">Oh no! Please tell us what went wrong!</h1>
           <h1 className="flex justify-center">Contact us at washroomwayfinder@gmail.com</h1>
           <h2 className="flex justify-center">Alternatively, make a GitHub issue at https://github.com/DoiBoi/nwPlus-Project/issues</h2>
-          <h1 className="flex justify-center mt-[10px] underline cursor-pointer" onClick={() => {
+          <h1 className="flex justify-center mt-[30px] underline cursor-pointer" onClick={() => {
               props.setShowError(false);
           }}>Click here to close</h1>
       </div>
